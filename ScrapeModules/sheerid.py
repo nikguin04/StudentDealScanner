7daeb6cce4ef7aed4a1c5781ac5d2b1eea38b0ff
nikguin04 <niklaspj2004@gmail.com>
2023-08-03 23:12:09 +0200

    Ported to python310

--- ScrapeModules/sheerid.py
+++ ScrapeModules/sheerid.py
@@ -49,16 +49,16 @@ def ScrapeNextPage(jsnScrape, pagenum):
         discount_links = {}
         for link in details_div.findAll("div", "single-btn-wrapper"):
             ahref = link.contents[1]
-            discount_links[str(ahref.encode_contents()).strip()] = ahref['href']
+            discount_links[str(ahref.contents).strip()] = ahref['href']
         jsnScrape['dealdata'].append({
             "name": citem['data-brands'],
-            "title": details_div.find("h1", "product_title").encode_contents(),
-            "desc_long": massReplaceStr(str(details_div.find("div", "woocommerce-product-details__short-description").encode_contents()), {"<p>":"", "</p>":"\n", "<br/>":"\n", "<h2>":"## ", "</h2>":"\n"}), # markdown readability
+            "title": details_div.find("h1", "product_title").contents,
+            "desc_long": massReplaceStr(str(details_div.find("div", "woocommerce-product-details__short-description").contents), {"<p>":"", "</p>":"\n", "<br/>":"\n", "<h2>":"## ", "</h2>":"\n"}), # markdown readability
             "discount_link_provider": details_link,
             "discount_links": discount_links,
             "images": [(details_div.find_all("img", "size-shop_single")[1]['src'] if details_div.find("img", "size-shop_single") != None else '')] # using find_all with index 1 because html parser sees no script as queryable html
         })
-        print("Scraped data for: " + citem['data-brands'].encode("utf-8"))
+        print("Scraped data for: " + str(citem['data-brands'].encode("utf-8")))
 
 def massReplaceStr(str, jsndict): 
     for key in jsndict.keys():
